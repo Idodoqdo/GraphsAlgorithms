a3b855071c521f9ac549b15a7006594c0b0946f4
Lshiela <lshiela@student.21-school.ru>
2022-12-05 20:34:31 +0700

    start

---
+++ src/s21_graph_algorithms/TravelingSalesmanProblem.cc
@@ -0,0 +1,8 @@
+#include "s21_graph_algorithms.h"
+namespace s21 {
+TsmResult GraphAlgorithms::solveTravelingSalesmanProblem(std::vector<std::vector<bool>> & graph) {
+    
+}
+
+
+}
--- src/s21_graph_algorithms/s21_graph_algorithms.h
+++ src/s21_graph_algorithms/s21_graph_algorithms.h
@@ -1,6 +1,13 @@
 // Copyright <lwolmer, lshiela, jgerrick> 2022
+#include <vector>
 namespace s21 {
-class GraphAlgorithms () {
-
-}
+struct TsmResult {
+std::vector<int> vertices;
+double distance;
+};
+class GraphAlgorithms {
+    public:
+    TsmResult solveTravelingSalesmanProblem(std::vector<std::vector<bool>> &graph);
+    private:
+};
 }
