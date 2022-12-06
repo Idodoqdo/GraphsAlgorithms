6c704b3ae5398762d564830103f3e4e99d649c38
jgerrick <iwan.a.25@mail.ru>
2022-12-06 12:45:21 +0700

    fix

--- src/s21_graph_algorithms/Prim's_algorithm.cc
+++ src/s21_graph_algorithms/Prim's_algorithm.cc
@@ -5,18 +5,18 @@
 namespace s21 {
 Graph GraphAlgorithms::getLeastSpanningTree(Graph &graph) {
   int edge_count = 1;
-  int graph_size = graph.GetSize();
-  std::vector<bool> selected(graph_size + 1, false);
+  int graph_size = graph.Size();
+  std::vector<bool> selected(static_cast<std::size_t>(graph_size + 1), false);
   Graph result(graph_size);
   selected[1] = true;
   int y = 1;  //  row number
   int x = 1;  //  col number
   while (edge_count < graph_size) {
-    int min = std::numeric_limits::max();
+    int min = std::numeric_limits<int>::max();
     for (int i = 1; i <= graph_size; i++) {
-      if (selected[i]) {
+      if (selected[static_cast<std::size_t>(i)]) {
         for (int j = 1; j <= graph_size; j++) {
-          if (!selected[j] && graph(i, j) && graph(i, j) < min) {
+          if (!selected[static_cast<std::size_t>(j)] && graph(i, j) && graph(i, j) < min) {
               min = graph(i, j);
               y = i;
               x = j;
@@ -26,7 +26,7 @@ Graph GraphAlgorithms::getLeastSpanningTree(Graph &graph) {
     }
     result(x, y) = min;
     result(y, x) = min;
-    selected[x] = true;
+    selected[static_cast<std::size_t>(x)] = true;
     edge_count++;
   }
   return result;
