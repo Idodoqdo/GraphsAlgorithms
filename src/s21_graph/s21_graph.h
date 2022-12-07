a4d2cb8b9c1eee72b8ce36aa9d95693d9b5c6304
lwolmer <it.cups.54@gmail.com>
2022-12-08 02:00:19 +0700

    to dot and fixes

--- src/s21_graph/s21_graph.h
+++ src/s21_graph/s21_graph.h
@@ -4,17 +4,21 @@
 #include <string>
 #include <vector>
 #include <cassert>
+#include <map>
 
 namespace s21 {
 class Graph {
  public:
   using matrix = std::vector<std::vector<int>>;
   Graph() = delete;
-  explicit Graph(std::string filepath);
+  explicit Graph(const std::string &filepath);
   explicit Graph(int size);
   explicit Graph(int *matrix, int size);
 
   int& operator ()(int x, int y) {
+    return const_cast<int&>(const_cast<const Graph*>(this)->operator()(x, y));
+  }
+  const int& operator ()(int x, int y) const {
     int size = Size();
     assert(x >= 0 && y >= 0 && x < size && y < size);
     return matrix_[static_cast<std::size_t>(y)][static_cast<std::size_t>(x)];
@@ -25,16 +29,17 @@ class Graph {
 
     @param filename Absolute Path to file
   */
-  void LoadGraphFromFile(std::string filename);
+  void LoadGraphFromFile(const std::string& filename);
   /**
       @brief Exporting a graph to a dot file (see materials)
       
       @param filename Absolute Path to file
   */
-  void ExportGraphToDot(std::string filename);
+  void ExportGraphToDot(const std::string& filename) const;
  private:
   void AllocateMatrix(int size);
-  bool CheckFormat(std::ifstream &stream, std::string reg);
+  bool CheckFormat(std::ifstream &stream, std::string reg) const;
+  std::multimap<int, std::pair<int, int>> GetAllPathsSortedByWeight() const;
   matrix matrix_;
 };
 }
--- src/s21_graph/tests/config.h
+++
@@ -1,7 +0,0 @@
-// Copyright <lwolmer, lshiela, jgerrick> 2022
-#ifndef SRC_TESTS_CONFIG_H_
-#define SRC_TESTS_CONFIG_H_
-
-#define CMAKE_CURRENT_SOURCE_DIR "/root/A2_SimpleNavigator_v1.0-0/src/s21_graph/tests"
-
-#endif  // SRC_TESTS_CONFIG_H_
