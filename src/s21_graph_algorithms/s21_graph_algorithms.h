93249471e0cf4078ac344bd98ed6d665a1a69694
jgerrick <iwan.a.25@mail.ru>
2022-12-07 12:31:33 +0700

    Dijkstra’s Shortest Path Algorithm

--- src/s21_graph_algorithms/s21_graph_algorithms.h
+++ src/s21_graph_algorithms/s21_graph_algorithms.h
@@ -1,6 +1,11 @@
 // Copyright <lwolmer, lshiela, jgerrick> 2022
-namespace s21 {
-class GraphAlgorithms () {
+#include <bits/stdc++.h>
 
-}
-}
+#include "../s21_graph/s21_graph.h"
+
+namespace s21 {
+class GraphAlgorithms {
+ public:
+  int getShortestPathBetweenVertices(Graph &graph, int vertex1, int vertex2);
+};
+}  // namespace s21
