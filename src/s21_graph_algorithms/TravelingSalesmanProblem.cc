edfc5c90cab9c12c2225b122fff7e55c018abb12
Lshiela <lshiela@student.21-school.ru>
2022-12-08 15:24:59 +0700

    Run

--- src/s21_graph_algorithms/TravelingSalesmanProblem.cc
+++ src/s21_graph_algorithms/TravelingSalesmanProblem.cc
@@ -9,23 +9,47 @@ void Colony::CreateAnt(int index_start) {
     ants_.push_back(std::make_unique<Ant>(distance_between_points_graph_, pheromones_graph_, index_start));
 }
 
-void Ant::TransitionDesireCalculation() {
-    std::vector<int> can_visit{};
-    for (auto j = 0; j < graph_distance_.Size(); ++j) {
-        if (std::find(tabu_.begin(), tabu_.end(), j) == tabu_.end()) {
+void Ant::TransitionDesireCalculation(std::vector<double> & transition_probabilitys_vec) {
+    for (auto j = available_places_.begin(); j != available_places_.end(); ++j) {
         double desire = 0;
         double sum_all_desires = 0;
-        CalculateDesire(j, desire);
-        for (auto m = 0; m < graph_distance_.Size(); ++m) {
-            if (std::find(tabu_.begin(), tabu_.end(), j) == tabu_.end()) CalculateDesire(m, sum_all_desires);
+        CalculateDesire(*j, desire);
+        for (auto m = available_places_.begin(); m != available_places_.end(); ++m) {
+            CalculateDesire(*m, sum_all_desires);
         }
-        transition_probabilitys_vec_.push_back(desire/sum_all_desires);
+        transition_probabilitys_vec.push_back(desire/sum_all_desires);
+    }
+}
+
+void Ant::Run() {
+    for (auto k = 0; k < graph_distance_.Size(); ++k) {
+        if (k == graph_distance_.Size() - 1) available_places_.insert(tabu_[0]);
+        std::vector<double> trans_prob{};
+        TransitionDesireCalculation(trans_prob);
+        Transition(trans_prob);
+    }
+}
+
+void Ant::Transition(std::vector<double> & transition_probabilitys_vec) {
+    double random_choice = distrib_(gen_);
+    for (unsigned i = 0; i < transition_probabilitys_vec.size(); ++i) {
+        if (random_choice <= transition_probabilitys_vec[i]) {
+            auto it = available_places_.begin();
+            std::advance(it, i);
+            tabu_.push_back(*it);
+            available_places_.erase(it);
+            break;
         }
     }
 }
 
-void Ant::CalculatingCitiesToVisit(std::vector<int> & vec) {
-
+void Ant::FillAvailablePlaces() {
+    for (auto i = 0; i < graph_distance_.Size(); ++i) {
+        if (i != tabu_[0]) {
+            available_places_.insert(i);
+        }
+    }
+    // available_places_.erase(available_places_.find(tabu_[0]));  - оставим под вопросом
 }
 
 void Ant::CalculateDesire(int index, double & result) {
--- src/s21_graph_algorithms/TravelingSalesmanProblem.h
+++ src/s21_graph_algorithms/TravelingSalesmanProblem.h
@@ -2,7 +2,10 @@
 #include "../s21_graph/s21_graph.h"
 #include <memory>
 #include <cmath>
-#include <algorithm>
+#include <random>
+#include <set>
+
+
 
 
 namespace s21 {
@@ -18,23 +21,34 @@ double beta_coeff = 1;
 class Ant {
     public:
     Ant(const Graph & graph_distance, std::unique_ptr<Graph> & graph_pheromones, int index_start) : graph_distance_(graph_distance), 
-    graph_pheromones_(graph_pheromones) {
+    graph_pheromones_(graph_pheromones), gen_(rd_()), distrib_(0, 1) {
         tabu_.push_back(index_start);
+        FillAvailablePlaces();
     };
     Ant() = delete;
+    // пробег муравья по всем городам
+    void Run();
+
     private:
+    // переход в другой город
+    void Transition(std::vector<double> & transition_probabilitys_vec);
     // расчёт вероятности перехода
-    void TransitionDesireCalculation();
+    void TransitionDesireCalculation(std::vector<double> & transition_probabilitys_vec);
     // расчет желания перехода в другой город
     void CalculateDesire(int index, double & result);
     // посчитаем города, куда он может ещё пойти
-    void CalculatingCitiesToVisit(std::vector<int> & vec);
     const Graph & graph_distance_;
     std::unique_ptr<Graph> & graph_pheromones_;
     // куда муравей уже заходил
     std::vector<int> tabu_{};
-    // вектор вероятностей перехода
-    std::vector<double> transition_probabilitys_vec_ {};
+    // куда муравей может зайти
+    std::set<int> available_places_{};
+    // заполнение индексов мест, куда можно пойти
+    void FillAvailablePlaces();
+    // для генерации случайного числа
+    std::random_device rd_;
+    std::mt19937 gen_;
+    std::uniform_real_distribution<double> distrib_;
 };
 
 class Colony {
