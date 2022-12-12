828270c15594e395399c476be8a10c6c8d28a6c0
Lshiela <lshiela@student.21-school.ru>
2022-12-12 20:13:09 +0700

    add test

--- src/s21_graph_algorithms/tests/traveling_test.cc
+++ src/s21_graph_algorithms/tests/traveling_test.cc
@@ -2,10 +2,24 @@
 #include "s21_graph_algorithms.h"
 #include "config.h"
 
-TEST(graph_algorithms, Ant) {
+// TEST(graph_algorithms, Ant) {
+//   s21::TsmResult result{};
+//   s21::Graph graph(CMAKE_CURRENT_SOURCE_DIR"/graph_raw_file");
+//   s21::GraphAlgorithms testAlg;
+//   result = testAlg.solveTravelingSalesmanProblem(graph);
+//   EXPECT_EQ(result.distance, 253);
+// }
+
+TEST(graph_algorithms, Ant2) {
   s21::TsmResult result{};
-  s21::Graph graph(CMAKE_CURRENT_SOURCE_DIR"/graph_raw_file");
+  double matrix[] = {
+    0, 10, 15, 20,
+    10, 0, 35, 25,
+    15, 35, 0, 30,
+    20, 25, 30, 0
+  };
+  s21::Graph graph(matrix, 4);
   s21::GraphAlgorithms testAlg;
   result = testAlg.solveTravelingSalesmanProblem(graph);
-  EXPECT_EQ(result.distance, 253);
+  EXPECT_EQ(result.distance, 80);
 }
