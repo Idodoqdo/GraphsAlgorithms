35bd21d436d33cc93786db77713442b2c9ceda93
lwolmer <lwolmer@student.21-school.ru>
2022-12-13 20:48:50 +0700

    name fixes

--- src/s21_graph_algorithms/search_algorithms.cc
+++ src/s21_graph_algorithms/search_algorithms.cc
@@ -9,18 +9,18 @@ std::vector<std::size_t> GraphAlgorithms::DepthFirstSearch(Graph &graph, int sta
   startVertex--; // cuz readme constrains
   if (startVertex >= static_cast<int>(graph.Size()) || startVertex < 0)
     throw std::invalid_argument("incorrect startVertex argument");
-  stack<std::size_t> stack;
+  stack<std::size_t> not_visited;
   std::vector<std::size_t> visited;
-  stack.push(static_cast<std::size_t>(startVertex));
-  while (!stack.empty()) {
-      std::size_t step = stack.top();
-      stack.pop();
-      visited.push_back(step + 1);
-      auto connected = graph.GetConnectedNodes(step);
+  not_visited.push(static_cast<std::size_t>(startVertex));
+  while (!not_visited.empty()) {
+      std::size_t step_pos = not_visited.top();
+      not_visited.pop();
+      visited.push_back(step_pos + 1);
+      auto connected = graph.GetConnectedNodes(step_pos);
       for (auto it = connected.rbegin(); it != connected.rend(); ++it) {
         // push only not visited
         if (std::find(visited.begin(), visited.end(), *it + 1) == visited.end())
-          stack.push(*it);
+          not_visited.push(*it);
       }
   }
   return visited;
