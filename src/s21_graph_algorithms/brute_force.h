66b0a447481c2942c1318381b9ca9d3654838120
lwolmer <lwolmer@student.21-school.ru>
2022-12-27 00:34:08 +0700

    egor's method

--- src/s21_graph_algorithms/brute_force.h
+++ src/s21_graph_algorithms/brute_force.h
@@ -11,39 +11,30 @@ class BruteForce {
   explicit BruteForce(const Graph& graph) : graph_(graph) {}
   void FindResult() {
     result_.distance = std::numeric_limits<double>::max();
-    for (std::size_t i = 0; i < graph_.Size(); ++i) {
-      RouteGeneration(i);
-    }
-  }
-  void RouteGeneration(std::size_t step, std::vector<std::size_t> route = {}, std::set<std::size_t> visited = {}) {
-    route.push_back(step);
-    visited.insert(step);
-    auto connected = graph_.GetConnectedNodes(step);
-    for(auto node : connected) {
-      if (step == node || graph_(step, node) < std::numeric_limits<double>::epsilon())
-        continue;
-      if (visited.size() == graph_.Size()) {
-        if (node == route.front()) {
-          route.push_back(route.front());
-          CalculateAndComparePath(route);
-          return;
-        }
-      } else {
-        if(visited.find(node) == visited.end())
-          RouteGeneration(node, route, visited);
+    for (std::size_t start = 0; start < graph_.Size(); ++start) {
+      std::vector<std::size_t> result = {};
+      for (std::size_t i = 0; i < graph_.Size(); ++i) {
+        result.push_back(i);
       }
-    }
-  };
-  void CalculateAndComparePath(std::vector<std::size_t> &route) {
-    double distance = 0;
-    for(auto i = route.begin(); i != route.end(); ++i) {
-      auto next = std::next(i);
-      if (next != route.end())
-        distance += graph_(*i, *next);
-    }
-    if (result_.distance > distance) {
-      result_.distance = distance;
-      result_.vertices = route;
+      result.push_back(start);
+      while (std::next_permutation(result.begin(),
+                                  result.end())) {
+        if (*result.begin() != *std::prev(result.end()))
+          continue;
+        std::size_t j = start;
+        double temp_path = 0;
+        for (std::size_t i = 1; i < result.size(); ++i) {
+          if (graph_(j, result[i]) > 0) {
+            temp_path += graph_(j, result[i]);
+            j = result[i];
+          } else {
+            temp_path = std::numeric_limits<double>::max();
+            break;
+          }
+        }
+        temp_path += graph_(j, start);
+        result_.distance = std::min(result_.distance, temp_path);
+      };
     }
   }
   TsmResult result() const { return result_; }
